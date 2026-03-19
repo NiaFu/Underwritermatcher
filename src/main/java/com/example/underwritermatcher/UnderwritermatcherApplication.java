3c6ce9f1b6b24a4cee0d384314e3a0646ebb438c
NiaFu <nia@victz.com>
2026-03-19 20:21:13 +1100

    Initial project setup

---
+++ src/main/java/com/example/underwritermatcher/UnderwritermatcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.underwritermatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderwritermatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnderwritermatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/underwritermatcher/UnderwritermatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.underwritermatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderwritermatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
